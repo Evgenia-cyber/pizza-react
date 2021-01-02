7df8a66c09bb2aba6f77537498d96639a66acc3b
Evgenia Kurineva <kurineva85@mail.ru>
2021-01-02 18:38:48 +0300

    Version 5.0: get data from public/db.json using fetch(). Create <PizzaBlock/>: available, active, disable types and sizes of pizzas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 
 import { Header } from './components';
@@ -6,11 +6,19 @@ import Cart from './pages/Cart';
 import Home from './pages/Home';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then((json) => setPizzas(json.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home pizzas={pizzas} />} />
         <Route path="/cart" component={Cart} />
       </div>
     </div>
