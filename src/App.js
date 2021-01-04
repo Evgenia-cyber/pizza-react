078674cf6b51377db11abd398c798a076f77269d
Evgenia Kurineva <kurineva85@mail.ru>
2021-01-04 13:06:20 +0300

    Version 9.0: converting <App/> from class component to a functional component: use hooks from react-redux (useDispatch() and useSelector()) instead of connect()

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Header } from './components';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
 import { setPizzas } from './redux/reducers/pizzasReducer';
-import { connect } from 'react-redux';
 
-class App extends React.Component {
-  componentDidMount() {
+function App() {
+  const dispatch = useDispatch();
+  const { pizzas } = useSelector((state) => ({
+    pizzas: state.pizzasReducer.pizzas,
+  }));
+
+  React.useEffect(() => {
     axios
       .get('http://localhost:3000/db.json')
-      .then((response) => this.props.setPizzas(response.data.pizzas));
-  }
+      .then((response) => dispatch(setPizzas(response.data.pizzas)));
+  }, [dispatch]);
 
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Route
-            exact
-            path="/"
-            render={() => <Home pizzas={this.props.pizzas} />}
-          />
-          <Route path="/cart" component={Cart} />
-        </div>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route exact path="/" render={() => <Home pizzas={pizzas} />} />
+        <Route path="/cart" component={Cart} />
       </div>
-    );
-  }
+    </div>
+  );
 }
+export default App;
 
-const mapStateToProps = (state) => ({
-  pizzas: state.pizzasReducer.pizzas,
-});
-// const mapDispatchToProps = (dispatch) => ({
-//   setPizzas: (pizzas) => {
-//     dispatch(setPizzas(pizzas));
-//   },
+// class App extends React.Component {
+//   componentDidMount() {
+//     axios
+//       .get('http://localhost:3000/db.json')
+//       .then((response) => this.props.setPizzas(response.data.pizzas));
+//   }
+//   render() {
+//     return (
+//       <div className="wrapper">
+//         <Header />
+//         <div className="content">
+//           <Route
+//             exact
+//             path="/"
+//             render={() => <Home pizzas={this.props.pizzas} />}
+//           />
+//           <Route path="/cart" component={Cart} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => ({
+//   pizzas: state.pizzasReducer.pizzas,
 // });
-const mapDispatchToProps = {
-  setPizzas,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// // const mapDispatchToProps = (dispatch) => ({
+// //   setPizzas: (pizzas) => {
+// //     dispatch(setPizzas(pizzas));
+// //   },
+// // });
+// const mapDispatchToProps = {
+//   setPizzas,
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
